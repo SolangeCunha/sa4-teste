e9390ffcc08b4a35ead4f5eb42f6d042c671fd8a
Solange Cunha <solfloripa.cunha@gmail.com>
2024-03-27 23:37:52 -0300

    Correções: Descreva suas alterações aqui

--- page_app/urls.py
+++ page_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from page_app.views import index, contato
+from page_app.views import index, contato, service
 
 urlpatterns = [
     # Cadastrar URLs aqui
     path('', index),
 
     path('contato/', contato),
+    path('services/', service),
+    
 ]
